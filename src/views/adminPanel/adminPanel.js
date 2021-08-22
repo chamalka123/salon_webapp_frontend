b1b731e35798772bf967b063302dde08d53d869e
saduni <saduniudeshika0@gmail.com>
2021-08-22 11:42:39 +0530

    Update admin panel props details

--- src/views/adminPanel/adminPanel.js
+++ src/views/adminPanel/adminPanel.js
@@ -3,42 +3,22 @@ import "./adminPanel.css";
 import Card from "../../components/Card/Card";
 import cardDetails from "../../components/Card/cardDetails"
 
+
+function createCard(cardDetails){
+    return(
+        <Card 
+            key = {cardDetails.id}
+            name = {cardDetails.name}
+            img  = {cardDetails.img}
+        />
+    );
+}
 function Admin(){
     return(
         <div className="admin-home">
             <h1>Admin Panel</h1>
-            <Card 
-                img = {cardDetails[0].img}
-                name = {cardDetails[0].name}
-            />
-            <Card 
-                img = {cardDetails[1].img}
-                name = {cardDetails[1].name}
-            />
-            <Card 
-                img = {cardDetails[2].img}
-                name = {cardDetails[2].name}
-            />
-                        <Card 
-                img = {cardDetails[2].img}
-                name = {cardDetails[2].name}
-            />
-                        <Card 
-                img = {cardDetails[2].img}
-                name = {cardDetails[2].name}
-            />
-                        <Card 
-                img = {cardDetails[2].img}
-                name = {cardDetails[2].name}
-            />
-                        <Card 
-                img = {cardDetails[2].img}
-                name = {cardDetails[2].name}
-            />
-                        <Card 
-                img = {cardDetails[2].img}
-                name = {cardDetails[2].name}
-            />
+            {cardDetails.map(createCard)}
+            
         </div>
     );
 }
