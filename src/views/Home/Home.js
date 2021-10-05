4d889d0c5bce07695f617620c3847347d4079718
saduni <saduniudeshika0@gmail.com>
2021-10-05 10:22:49 +0530

    Home page changed

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./Home.css";
+import Card from "../../components/Card/Card";
+import customerCardDetails from "../../components/Card/customerCardDetails";
 
 function Home(){
-    return(
-        <div className="home-home">
-            
-            <h1>Home</h1>
-            </div>
-    );
+    return (
+        <div className="admin-home">
+            <div class="heading">Home</div>
+          {customerCardDetails.map((customerCardDetails) => {
+            return (
+              <div>
+                <Card
+                  key={customerCardDetails.id}
+                  name={customerCardDetails.name}
+                  img={customerCardDetails.img}
+                  navigate={customerCardDetails.navigate}
+                />
+              </div>
+            );
+          })}
+        </div>
+      );
 }
 export default Home;
